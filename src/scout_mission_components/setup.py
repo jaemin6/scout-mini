f2070c178aac9f6487280b18aabc091d774642d0
eddy <gumt555@gmail.com>
2025-11-11 10:56:47 +0900

    launch 파일 생성, setup.py에 launch 파일 추가

--- src/scout_mission_components/setup.py
+++ src/scout_mission_components/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob # glob 임포트 추가
+import os
 
 package_name = 'scout_mission_components'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,

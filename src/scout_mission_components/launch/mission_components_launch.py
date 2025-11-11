b438015658252b0257c845a3129910b26938f91b
eddy <gumt555@gmail.com>
2025-11-11 17:12:23 +0900

    WIP: Container component conversion changes

--- src/scout_mission_components/launch/mission_components_launch.py
+++ src/scout_mission_components/launch/mission_components_launch.py
@@ -2,66 +2,30 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    
-    # 1. 🏭 컴포넌트를 담을 컨테이너 노드 정의 (mission_component_container)
-    # rclcpp_components 패키지의 component_container_mt를 사용해 멀티스레드 환경을 구축합니다.
-    container = Node(
-        package='rclcpp_components',
-        executable='component_container_mt',
-        name='mission_component_container',
-        output='screen',
-        emulate_tty=True,
-    )
-
-    # 2. 컴포넌트 로드 정의
-    
-    # 로딩하려는 각 컴포넌트는 'Node' 액션을 사용하며, 
-    # 'executable'은 setup.py의 'rclpy_components'에 등록된 이름(왼쪽 값)을 사용합니다.
-    
-    # 2-1. AmclResetter 컴포넌트 로드
-    amcl_reset_node = Node(
-        package='scout_mission_components',
-        executable='amcl_reset', # setup.py에 등록된 이름
-        name='amcl_resetter',
-        output='screen',
-        emulate_tty=True,
-        container='mission_component_container', # 컨테이너 이름 지정
-    )
-
-    # 2-2. RoomNavigator (Nav2 Commander) 컴포넌트 로드
-    nav2_commander_node = Node(
-        package='scout_mission_components',
-        executable='nav2_commander', # setup.py에 등록된 이름
-        name='room_navigator',
-        output='screen',
-        emulate_tty=True,
-        container='mission_component_container',
-    )
-    
-    # 2-3. QrDetector 컴포넌트 로드
-    qr_detector_node = Node(
-        package='scout_mission_components',
-        executable='qr_detector', # setup.py에 등록된 이름
-        name='qr_detector',
-        output='screen',
-        emulate_tty=True,
-        container='mission_component_container',
-    )
-    
-    # 2-4. RobotRotator 컴포넌트 로드
-    robot_rotator_node = Node(
-        package='scout_mission_components',
-        executable='robot_rotator', # setup.py에 등록된 이름
-        name='robot_rotator',
-        output='screen',
-        emulate_tty=True,
-        container='mission_component_container',
-    )
-
     return LaunchDescription([
-        container,
-        amcl_reset_node,
-        nav2_commander_node,
-        qr_detector_node,
-        robot_rotator_node,
+        Node(
+            package='scout_mission_components',
+            executable='qr_detector',
+            name='qr_detector',
+            output='screen'
+        ),
+        Node(
+            package='scout_mission_components',
+            executable='robot_rotator_node',
+            name='robot_rotator',
+            output='screen'
+        ),
+        Node(
+            package='scout_mission_components',
+            executable='amcl_reset_node',
+            name='amcl_resetter',
+            output='screen'
+        ),
+        Node(
+            package='scout_mission_components',
+            executable='nav2_commander',
+            name='room_navigator',
+            output='screen'
+        ),
     ])
+
--- src/scout_mission_components/setup.py
+++ src/scout_mission_components/setup.py
@@ -1,42 +1,31 @@
-from setuptools import find_packages, setup
-from glob import glob # glob 임포트 추가
-import os
+from setuptools import setup
 
 package_name = 'scout_mission_components'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
+        ('share/' + package_name + '/launch', ['launch/mission_components_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='eddy',
-    maintainer_email='gumt555@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer_email='eddy@example.com',
+    description='Mission components for Scout robot',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 기존 독립 실행 스크립트가 없으므로 비워둡니다.
-        ],
-        # ----------------------------------------------------
-        # 🌟🌟 파이썬 컴포넌트 등록 섹션 (필수) 🌟🌟
-        # ----------------------------------------------------
-        'rclpy_components': [
-            'amcl_reset = scout_mission_components.amcl_reset:create_node',
-            'nav2_commander = scout_mission_components.nav2_commander1:create_node',
-            'qr_detector = scout_mission_components.qr_detector:create_node',
-            'robot_rotator = scout_mission_components.robot_rotator:create_node',
+            'qr_detector = scout_mission_components.qr_detector:main',
+            'robot_rotator_node = scout_mission_components.robot_rotator_node:main',
+            'amcl_reset_node = scout_mission_components.amcl_reset_node:main',
+            'nav2_commander = scout_mission_components.nav2_commander:main',
         ],
     },
 )
+

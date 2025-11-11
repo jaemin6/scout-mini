1e07429c9ea364dcffd1e812037dc7675c405a38
eddy <gumt555@gmail.com>
2025-11-11 10:51:00 +0900

    컴포넌트 인식을 위한 setup.py 수정

--- src/scout_mission_components/setup.py
+++ src/scout_mission_components/setup.py
@@ -24,6 +24,16 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            # 기존 독립 실행 스크립트가 없으므로 비워둡니다.
+        ],
+        # ----------------------------------------------------
+        # 🌟🌟 파이썬 컴포넌트 등록 섹션 (필수) 🌟🌟
+        # ----------------------------------------------------
+        'rclpy_components': [
+            'amcl_reset = scout_mission_components.amcl_reset:create_node',
+            'nav2_commander = scout_mission_components.nav2_commander1:create_node',
+            'qr_detector = scout_mission_components.qr_detector:create_node',
+            'robot_rotator = scout_mission_components.robot_rotator:create_node',
         ],
     },
 )
